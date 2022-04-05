c606a74c97b79f9d5d406d07619feb4de35a88fc
Gregory Suvalian <gregory.suvalian@intapp.com>
2022-04-05 12:00:06 -0500

    Added SQL

---
+++ AKSvsAppService/AzurePerformance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+var client = new System.Net.Http.HttpClient();
+var tasks = new List<Task<int>>();
+var watch = System.Diagnostics.Stopwatch.StartNew();
+for (int i = 1; i < 101; i++)
+{
+   async Task<int> func()
+   {
+
+      string connString = "Server=someserver.database.usgovcloudapi.net;Database=Someserver;user id=core360user;password=somepassword;MultipleActiveResultSets=True;Pooling=false;";
+      SqlConnection connection = new SqlConnection(connString);
+      SqlCommand command = new SqlCommand("Select @@VERSION", connection);
+      command.Connection.Open();
+      //var response = await client.GetAsync("http://1.1.1.1");
+      //var reader = await command.ExecuteNonQueryAsync();
+      return await command.ExecuteNonQueryAsync();
+   }
+
+   tasks.Add(func());
+}
+
+await Task.WhenAll(tasks);
+watch.Stop();
+Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
