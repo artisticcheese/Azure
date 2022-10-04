b3d9b28f93f8c910637a3d7dccb73814187288d4
GSA <artisticcheese@gmail.com>
2022-10-04 10:59:06 -0500

    Update ETC-2miners.sh

--- ACI-Mining/trex/ETC-2miners.sh
+++ ACI-Mining/trex/ETC-2miners.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./t-rex -a etchash -o stratum+tcp://etc.2miners.com:1010 -u 0x15cbe037E6E94e9a99b5f125E2a806E58A248b75 -p x -w rig0 --api-bind-http 0.0.0.0:80 --api-read-only
+./t-rex -a etchash -o stratum+tcp://etc.2miners.com:1010 -u 0x6c9d01be09ebab8fd6531ab774c98ca89e964b0b -p x -w rig0 --api-bind-http 0.0.0.0:80 --api-read-only
